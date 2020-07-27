1612b6fc4fe483843c3af40ee21dfe0b08cdba0d
gustavo-vasconcellos <gustavo.vasconcellos@acct.global>
2020-07-27 17:23:52 -0300

    Implement divisor

--- react/FormattedCurrency.tsx
+++ react/FormattedCurrency.tsx
@@ -3,23 +3,32 @@ import { useRuntime } from 'vtex.render-runtime'
 import { useIntl } from 'react-intl'
 import formatCurrency from './formatCurrency'
 
-const FormattedCurrency: FC<FormattedCurrencyProps> = ({
-  value,
-}) => {
+interface FormattedCurrencyProps {
+  value: number
+  divisor?: string
+}
+
+const FormattedCurrency: FC<FormattedCurrencyProps> = ({ value, divisor }) => {
   const { culture } = useRuntime()
   const intl = useIntl()
 
   const number = formatCurrency({ intl, culture, value })
 
-  return (
-    <Fragment>
-      {number}
-    </Fragment>
-  )
-}
+  if (divisor) {
+    const [integer, decimal] = number.split(divisor)
 
-interface FormattedCurrencyProps {
-  value: number
+    return (
+      <Fragment>
+        <span>{integer}</span>
+        <span>
+          {divisor}
+          {decimal}
+        </span>
+      </Fragment>
+    )
+  }
+
+  return <Fragment>{number}</Fragment>
 }
 
 export default FormattedCurrency
